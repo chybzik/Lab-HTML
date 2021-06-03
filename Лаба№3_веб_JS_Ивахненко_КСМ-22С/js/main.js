572e1f75381896794ec38bf98b79214f7e8ed40c
chybzik <83736108+chybzik@users.noreply.github.com>
2021-06-03 20:29:22 +0300

    Add files via upload

---
+++ Лаба№3_веб_JS_Ивахненко_КСМ-22С/js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+function calc(first, second, operation) {
+  switch (operation) {
+    case '+':
+      return first + second;
+    case '-':
+      return first - second;
+    case '*':
+      return first * second;
+    case '/':
+      return first / second;
+  }
+}
+
+let first ='';
+let second = '';
+let operation = '';
+
+while (first == '' || second == '' || operation == '')
+  {
+    first = prompt('Введите первое число', first);
+    second = prompt('Введите второче число', second);
+    operation = prompt('Введите знак', operation);
+  }
+
+alert(calc(first, second, operation));
